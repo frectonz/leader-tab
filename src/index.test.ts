96e52bdd2bfa4e66630aed13d8a61b93ecaeccc6
frectonz <fraol0912@gmail.com>
2025-04-15 11:36:38 +0300

    feat: add single leader tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,23 +5,44 @@ function sleep(ms: number): Promise<void> {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-test("test", async ({ onTestFailed, onTestFinished }) => {
-	const ops: string[] = [];
-
-	const { isLeader, destroy } = elector({
-		onLeaderDemoted() {
-			ops.push("demoted");
-		},
-		onLeaderElected() {
-			ops.push("elected");
-		},
-	});
+async function onlyOneLeader(count: number) {
+	const tabs = new Array(count).fill(null).map(() => elector());
 
 	await sleep(1000);
 
-	expect(isLeader()).toBe(true);
-	expect(ops).toEqual(["elected"]);
+	const oneLeader = tabs.filter((x) => x.isLeader()).length === 1;
+	expect(oneLeader).toBe(true);
 
+	return () => {
+		tabs.forEach((tab) => tab.destroy());
+	};
+}
+
+test("elects a single leader for 1 tab", async ({
+	onTestFailed,
+	onTestFinished,
+}) => {
+	const destroy = await onlyOneLeader(1);
+
+	onTestFailed(() => destroy());
+	onTestFinished(() => destroy());
+});
+
+test("elects a single leader for 2 tabs", async ({
+	onTestFailed,
+	onTestFinished,
+}) => {
+	const destroy = await onlyOneLeader(2);
+
+	onTestFailed(() => destroy());
+	onTestFinished(() => destroy());
+});
+
+test("elects a single leader for 10 tabs", async ({
+	onTestFailed,
+	onTestFinished,
+}) => {
+	const destroy = await onlyOneLeader(10);
 
 	onTestFailed(() => destroy());
 	onTestFinished(() => destroy());
